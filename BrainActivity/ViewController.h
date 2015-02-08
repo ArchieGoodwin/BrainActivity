cb6270945fef0a2748106686514e0ba95d2b89a3
ArchieGoodwin <nerowolfe@me.com>
2015-02-08 15:16:02 +0300

    Initial Commit

---
+++ BrainActivity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BrainActivity
+//
+//  Created by Nero Wolfe on 08/02/15.
+//  Copyright (c) 2015 Sergey Dikarev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
